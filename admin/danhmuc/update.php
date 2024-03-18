c1cb399fa6fcd55bc98f676fe27387c46eaac2cd
nguyendihoc <trannguyenab1235@gmail.com>
2024-03-18 13:48:11 +0700

    cap nhat updatePHP

--- admin/danhmuc/update.php
+++ admin/danhmuc/update.php
@@ -21,7 +21,7 @@ if(is_array($dm)){
         <input type="text" name="ten_danhmuc" value="<?php if(isset($ten_danhmuc)&&($ten_danhmuc!="")) echo $ten_danhmuc;?>">
       </div>
       <div class="form-group">
-        <input type="hidden" name="ma_danhmuc" value="<?php if(isset($ma_danhmuc)&&($ma_danhmuc>0)) echo $idma_danhmuc;?>">
+        <input type="hidden" name="ma_danhmuc" value="<?php if(isset($ma_danhmuc)&&($ma_danhmuc>0)) echo $ma_danhmuc;?>">
         <input type="submit" name="capnhat" value="Cập nhật">
         <input type="reset" value="Nhập lại">
         <a href="index.php?act=listdm"><input type="button" value="DANH SÁCH"></a>
@@ -30,6 +30,6 @@ if(is_array($dm)){
       if (isset($thongbao) && ($thongbao != ""))
         echo $thongbao; ?>
     </form>
-    </table>
+    </table>/
 </section>
 <!-- end thêm danh mục -->
